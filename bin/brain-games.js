0da74f31b113a73aaad1cdc9aaac54b069267194
Aleksandr <chebotarelli@gmail.com>
2023-01-29 20:39:27 +0300

    template update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,7 +2,7 @@
 
 import getName from '../src/cli.js';
 
-console.log('node bin/brain-games.js');
-console.log('Welcome to the Brain Games!');
+// console.log('node bin/brain-games.js');
+// console.log('Welcome to the Brain Games!');
 
-console.log(getName());
+getName();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   return console.log(`Hello, ${name}!`);
 };
